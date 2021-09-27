08494e4f0bd8cccd4e94c431033944082ba19316
Yuri Carvalho <yuri.carvalho@devintex.local>
2021-09-27 14:36:14 -0300

    teste de link

---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+function Sobre() {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <link href="/">
+                <a>Home</a>
+            </link>
+        </div>
+    )
+}
+
+export default Sobre
