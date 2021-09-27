612e9e1e31004def848edb88c3fa7885594d1e2d
Yuri Carvalho <yuri.carvalho@devintex.local>
2021-09-27 15:37:42 -0300

    Atualizar branch teste

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,44 @@
-import { useState } from 'react';
+import Link from 'next/link';
 
 function Home() {
     return (
         <div>
-            <h2>Página inicial </h2>
-            <Contador />
-        </div>
-    )
-}
+            <h1>Home</h1>
 
-function Contador() {
-    const [contador,setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 2);
-    }
-
-    return(
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
+            <Link href="/sobre">
+                <a>Sobre nós</a>
+            </Link>
         </div>
     )
 }
 
 export default Home
+
+
+//import { useState } from 'react';
+
+//function Home() {
+//    return (
+//        <div>
+//            <h2>home </h2>
+//            <Contador />
+//        </div>
+//    )
+//}
+
+//function Contador() {
+//    const [contador,setContador] = useState(1);
+//
+//    function adicionarContador() {
+//        setContador(contador + 2);
+//    }
+//
+//    return(
+//        <div>
+//            <div>{contador}</div>
+//            <button onClick={adicionarContador}>Adicionar</button>
+//        </div>
+//    )
+//}
+
+// export default Home
