8b19169df29eae4093928980a9c52a483f156a97
unknown <yuricarvalhooliveira@gmail.com>
2021-09-27 23:02:47 -0300

    novo topo

--- components/topo.js
+++ components/topo.js
@@ -1,19 +1,20 @@
 import Link from 'next/link';
 import styles from '../styles.module.css';
+import Image from 'next/image'
 
 function Topo() {
     return (
         <div>
-            <div>
-                <Link href="/">
-                    <a>LOGOTIPO</a>
-                </Link>
-            </div>
-            <div>
-                <div>
-                    <Link href="/"><a>Home</a></Link>
-                    <Link href="/sobre"><a>Sobre nós</a></Link>
-                    <Link href="/tempo"><a>Tempo</a></Link>
+            <div className={styles.areaTopo}>
+                <div class="logotipo">
+                    <Image src="/logo.png" alt="Logotipo ALT"  width="250" height="75" />
+                </div>
+                <div className={styles.areaMenu}>
+                    <ul className={styles.areaMenu.ul}>
+                        <li className={styles.areaMenu.li}><a href="/">Página inicial</a></li>
+                        <li className={styles.areaMenu.li}><a href="/sobre">Sobre nós</a></li>
+                        <li className={styles.areaMenu.li}><a href="/tempo">tempo</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
