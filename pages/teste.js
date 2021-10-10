af4ee4d075745c3af62346b864a2efa69fb93445
unknown <yuricarvalhooliveira@gmail.com>
2021-10-10 14:58:07 -0300

    console log da busca 2

--- pages/teste.js
+++ pages/teste.js
@@ -1,25 +1,25 @@
-import Link from 'next/link'
-
-function Index({ estrelas }) {
-  return (
-    <div>
-      <p>Next.js has {estrelas} ⭐️</p>
-      <Link href="/preact-stars">
-        <a>How about preact?</a>
-      </Link>
-    </div>
-  )
-}
-
-export async function getStaticProps() {
-  const res = await fetch('https://www.yurioliveira.tec.br/api/categorias')
-  const json = await res.json()
-
-  return {
-    props: {
-        estrelas: json.estrelas,
-    },
-  }
-}
-
-export default Index
+//import Link from 'next/link'
+//
+//function Index({ estrelas }) {
+//  return (
+//    <div>
+//      <p>Next.js has {estrelas} ⭐️</p>
+//      <Link href="/preact-stars">
+//        <a>How about preact?</a>
+//      </Link>
+//    </div>
+//  )
+//}
+//
+//export async function getStaticProps() {
+//  const res = await fetch('https://www.yurioliveira.tec.br/api/categorias')
+//  const json = await res.json()
+//
+//  return {
+//    props: {
+//        estrelas: json.estrelas,
+//    },
+//  }
+//}
+//
+//export default Index
