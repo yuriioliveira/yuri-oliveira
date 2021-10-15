c2b14eeb86a0bb332b8aab3c6b2a1396451f95ab
Yuri Carvalho <yuri.carvalho@devintex.local>
2021-10-15 15:29:26 -0300

    incluindo bd 01 V3

--- pages/teste.js
+++ pages/teste.js
@@ -1,26 +1,33 @@
 import Link from 'next/link'
 
-function testeApi({ estrelas }) {
+function testeApi() {
   return (
-    <div>
-      <p>Next.js has {estrelas} ⭐️</p>
-      <Link href="/preact-stars">
-        <a>How about preact?</a>
-      </Link>
-      <p>Lista das categorias aqui pra baixo Está comentado</p>
-    </div>
+    <h1>Página de teste</h1>
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch('https://www.yurioliveira.tec.br/api/categoriasfixa')
-  const json = await res.json()
 
-  return {
-    props: {
-        estrelas: json.estrelas,
-    },
-  }
-}
+// function testeApi({ estrelas }) {
+//   return (
+//     <div>
+//       <p>Next.js has {estrelas} ⭐️</p>
+//       <Link href="/preact-stars">
+//         <a>How about preact?</a>
+//       </Link>
+//       <p>Lista das categorias aqui pra baixo Está comentado</p>
+//     </div>
+//   )
+// }
+
+// export async function getStaticProps() {
+//   const res = await fetch('https://www.yurioliveira.tec.br/api/categoriasfixa')
+//   const json = await res.json()
+
+//   return {
+//     props: {
+//         estrelas: json.estrelas,
+//     },
+//   }
+// }
 
 export default testeApi
